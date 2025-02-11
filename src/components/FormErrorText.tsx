b5cff895aedb5a946561063a1783f11e892ba168
j.shkreli <j.shkreli@teamsystem.com>
2025-02-11 14:53:30 +0100

    Wrapped ErrorText into a separate component

---
+++ src/components/FormErrorText.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+import { Typography } from "@mui/material";
+import styles from "./FormErrorText.module.scss";
+
+interface FormErrorTextProps {
+    error?: string;
+}
+
+const FormErrorText: React.FC<FormErrorTextProps> = ({ error }) => {
+    if (!error) return null; // If no error, don't render anything
+
+    return <Typography className={styles.errorText}>{error}</Typography>;
+};
+
+export default FormErrorText;
